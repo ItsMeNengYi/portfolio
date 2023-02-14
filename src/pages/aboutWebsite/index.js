62623477373829049d09c5fbcbd750a37c4d6e29
ItsMeFaquu <20040408gry@gmail.com>
2023-02-14 16:14:36 +0800

    .

--- src/pages/aboutWebsite/index.js
+++ src/pages/aboutWebsite/index.js
@@ -3,6 +3,7 @@ import BorderCard from "@/components/BorderCard"
 import Page from "@/components/Page"
 import Navbar from "@/components/Navbar"
 import ContentContainer from "@/components/ContentContainer"
+import GitButton from "@/components/GitButton"
 
 const title={
     position: 'relative',
@@ -21,8 +22,15 @@ export default function AboutMe() {
             <Navbar sx={{top:"0"}}/>
             <BorderCard>
             <Typography sx={title} variant="h4">About this Website</Typography>
-                <Typography>
-                    This is my first time using nextjs to build a website. I learnt basic of html,js and css before but never build a website myself. With some aid of my brother, Im able to learn basic syntax of nextjs, use mui library to make my progress faster.
+            <GitButton url="https://github.com/ItsMeFaquu/portfolio"/>
+                <Typography paragraph="true">
+                    This is my first time using nextjs to build a website. I learnt basic of html,js and css before but never build a website myself. With some aid of my brother, Im able to learn basic syntax of nextjs, use mui library to make my progress faster. 
+                </Typography>
+                <Typography paragraph="true">
+                    I spent a lot of time reading my code since all of these project are done years ago and I have to think back what obstacles I've met. Im glad that I wrote some note for some of the projects. I didnt record my projects' progression hence there's no much video.
+                </Typography>
+                <Typography paragraph="true">
+                    !Most buttons are interactable even they dont look like they are!
                 </Typography>
             </BorderCard>
         </ContentContainer>
